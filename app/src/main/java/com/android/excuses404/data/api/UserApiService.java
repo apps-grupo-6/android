21ba46cfb78e7bbfa008134d4e6f443a1568b5eb
Luciana Bermudez <bermudezluciana@hotmail.com>
2025-09-23 22:25:24 -0300

    Update UserApiService.java

--- app/src/main/java/com/android/excuses404/data/api/UserApiService.java
+++ app/src/main/java/com/android/excuses404/data/api/UserApiService.java
@@ -1,7 +1,15 @@
 package com.android.excuses404.data.api;
 
+import com.android.excuses404.data.api.model.OtpVerificationRequest;
+import com.android.excuses404.data.api.model.OtpVerificationResponse;
+import com.android.excuses404.data.api.model.ResendOtpRequest;
+import com.android.excuses404.data.api.model.ResendOtpResponse;
+import com.android.excuses404.data.api.model.ResetPasswordRequest;
+import com.android.excuses404.data.api.model.ResetPasswordResponse;
 import com.android.excuses404.data.api.model.UserLoginRequest;
 import com.android.excuses404.data.api.model.UserLoginResponse;
+import com.android.excuses404.data.api.model.UserRegisterRequest;
+import com.android.excuses404.data.api.model.UserRegisterResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -10,4 +18,16 @@ import retrofit2.http.POST;
 public interface UserApiService {
     @POST("auth/")
     Call<UserLoginResponse> login(@Body UserLoginRequest loginRequest);
+
+    @POST("users/")
+    Call<UserRegisterResponse> register(@Body UserRegisterRequest registerRequest);
+
+    @POST("auth/verify")
+    Call<OtpVerificationResponse> verifyOtp(@Body OtpVerificationRequest otpRequest);
+
+    @POST("auth/resend-otp")
+    Call<ResendOtpResponse> resendOtp(@Body ResendOtpRequest resendRequest);
+
+    @POST("auth/reset-password")
+    Call<ResetPasswordResponse> resetPassword(@Body ResetPasswordRequest resetRequest);
 }
